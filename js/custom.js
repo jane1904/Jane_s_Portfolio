6653453623d094b8f7845280f3c9fe2df851a7c9
JANE NGUYEN <janenguyen19490@gmail.com>
2024-11-14 21:18:44 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Graphic Desinger.", "Illustrator."],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
+new WOW().init();
+
+
